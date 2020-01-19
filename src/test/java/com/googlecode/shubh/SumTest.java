c1293dd192ec4d9df14e16ac9ebc45382863cde1
Shubh2003 <59472112+Shubh2003@users.noreply.github.com>
2020-01-20 01:08:55 +0530

    Update SumTest.java

--- src/test/java/com/googlecode/shubh/SumTest.java
+++ src/test/java/com/googlecode/shubh/SumTest.java
@@ -14,6 +14,12 @@ public class SumTest {
 		int ret = obj.SumTotal(2, 3);
 		
 		Assert.assertEquals(5, ret);
+		
 	}
-
-}
+		
+	@Test
+	public void HelloWorldTest() {
+		Sum obj = new Sum();
+		 obj.HelloWorld();		
+		 Assert.assertTrue(true);
+}}
